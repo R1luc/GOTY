f1378adc59055ea0797536ffe928bcdb87891379
R1luc <ryan.lucas@escolar.ifrn.edu.br>
2025-12-23 15:15:18 -0300

    Não adiciona mais a imagem no banco

--- routes/gnomo.js
+++ routes/gnomo.js
@@ -3,7 +3,19 @@ let db = require('../utils/db')
 let router = express.Router();
 let multer = require('multer');
 
-let storage = multer.memoryStorage(); // mantém em memória (Buffer)
+const path = require('path');
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images');
+    },
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+        const nomeArquivo = Date.now() + '-' + Math.round(Math.random() * 1E9) + ext;
+        cb(null, nomeArquivo);
+    }
+});
+
 let upload = multer({ storage });
 
 router.get('/listar-gnomo', function(req, res) {
@@ -24,7 +36,7 @@ router.get('/add', function(req, res) {
 router.post('/add', upload.single('foto_gnomo'), function(req, res){
     var nome_gnomo = req.body.nome_gnomo;
     var desc_gnomo = req.body.desc_gnomo;
-    var foto_gnomo = req.file.buffer;
+    var foto_gnomo = req.file ? `/images/${req.file.filename}` : null;
 
     var cmd = `INSERT INTO tb_gnomo (nome_gnomo, desc_gnomo, foto_gnomo)
                 VALUES (?, ?, ?)`
@@ -34,21 +46,6 @@ router.post('/add', upload.single('foto_gnomo'), function(req, res){
             res.send(erro);
         }
         res.redirect('/gnomo/listar-gnomo');
-    });
-});
-
-router.get('/foto/:id', function(req, res) {
-    const id = req.params.id;
-
-    const cmd = 'SELECT foto_gnomo FROM tb_gnomo WHERE id_gnomo = ?';
-
-    db.query(cmd, [id], function(err, result) {
-        if (err) return res.sendStatus(500);
-        if (result.length === 0) return res.sendStatus(404);
-
-        // ⚠️ ajuste se for PNG
-        res.setHeader('Content-Type', 'image/jpeg');
-        res.end(result[0].foto_gnomo);
     });
 });
 
